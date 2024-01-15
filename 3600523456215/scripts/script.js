878aeabcfdb73647ebf2a17fddb51453dd4e8f8d
Roman Pavlenko <rpavlenko.mail@gmail.com>
2024-01-15 21:11:37 +0100

    fix: image and slider

--- 3600523456215/scripts/script.js
+++ 3600523456215/scripts/script.js
@@ -1,11 +1,11 @@
-// require(['jquery', 'slick'], function ($) {
-$(document).ready(function () {
-  $('.es-slick-carousel').slick({
-    dots: true,
-    infinite: true,
-    prevArrow: '.slick-button-prev',
-    nextArrow: '.slick-button-next',
-    appendDots: '.slick-pagination',
+require(['jquery', 'slick'], function ($) {
+  $(document).ready(function () {
+    $('.es-slick-carousel').slick({
+      dots: true,
+      infinite: true,
+      prevArrow: '.slick-button-prev',
+      nextArrow: '.slick-button-next',
+      appendDots: '.slick-pagination',
+    });
   });
 });
-// })
